1b2394f1dcacbeb11a150ad3bc1723317857804a
deathesther <122229257+Esther7171@users.noreply.github.com>
2024-01-05 23:52:04 +0530

    hii.sh

--- hii.sh
+++ hii.sh
@@ -5,13 +5,14 @@ underline="\e[4;32m"   # 4
 blink="\e[5;32m"       # 5
 highlight="\e[7;32m"   # 7
 ending="\e[0m"         # 0
-Red='\033[0;31m'          # Red   echo -e "\033[1;35m  \033[0m"
+Red='\033[1;31m'          # Red   echo -e "\033[1;35m  \033[0m"
 Green='\033[0;32m'        # Green
 Yellow='\033[0;33m'       # Yellow
 Blue='\033[0;34m'         # Blue
 Purple='\033[0;35m'       # Purple
 Cyan='\033[0;36m'          # light blue
-echo "EXTRACTING DRIVERS " 
+reset='\033[0m'
+echo -e "$Red EXTRACTING DRIVERS $reset" 
 sudo apt-get update -y
 cd /home/$USER/
 cd /home/$USER/kali-wifi-driver-and-wifi-hacking
@@ -23,10 +24,10 @@ sudo make load
 iwconfig
 echo "INSTALL DRIVERS FOR TP LINK"
 cd /home/$USER/
-sudo apt install dkms git
-sudo apt install build-essential libelf-dev linux-headers-$(uname -r)
+sudo apt install dkms git -y
+sudo apt install build-essential libelf-dev linux-headers-$(uname -r) -y
 git clone https://github.com/aircrack-ng/rtl8812au.git
 cd rtl88*
-sudo make dkms_install
+sudo make dkms_install -y
 lsusb
 init 6 
