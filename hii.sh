ee78453998b134e44159649856f307d9e9ae286f
deathesther <122229257+Esther7171@users.noreply.github.com>
2024-01-17 19:15:58 +0530

    hii.sh

--- hii.sh
+++ hii.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd kali-wifi-driver-and-wifi-hacking/
+cd Wifi_hacking
 tar -xjvf compat-wireless-2010-06-28.tar.bz2 
 cd compat-wireless-2010-06-28/    
 sudo make unload 
@@ -7,10 +7,9 @@ sudo make load
 sudo apt update -y 
 cd /home/$USER/
 sudo apt install dkms git
-sudo apt install build-essential libelf-dev linux-headers-$(uname -r) -y
+sudo apt install build-essential libelf-dev linux-headers-$(uname -r)
 git clone https://github.com/aircrack-ng/rtl8812au.git
 cd rtl88*
-sudo make dkms_install -y
+sudo make dkms_install
 lsusb
-rm -rf kali-wifi* rtl88*
 init 6
